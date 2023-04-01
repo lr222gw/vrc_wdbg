c691873c1891b0b90f44e6b61bf12d07286f5695
lr222gw <lowe.raivio@gmail.com>
2023-04-01 16:39:56 +0200

    init: Problem with the OnTriggerEnter/Exit is occuring

---
+++ Assets/Behaviors/onTrigger_script.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class onTrigger_script : UdonSharpBehaviour
+{
+    void Start()
+    {
+        // box.attachedRigidbody.detectCollisions = false;
+    }
+    [SerializeField]
+    private BoxCollider box;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(
+            "## ENTER Triggered     : " + other.GetInstanceID().ToString()  + "\n" + 
+            "## ENTER Player ID     : " + Networking.LocalPlayer.playerId.ToString() + "\n" + 
+            "## ENTER Is Cube Held? : " + other.GetComponent<VRC_Pickup>().IsHeld
+        );
+  
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        
+        Debug.Log(
+            "## EXIT Triggered     : " + other.GetInstanceID().ToString()  + "\n" + 
+            "## EXIT Player ID     : " + Networking.LocalPlayer.playerId.ToString() + "\n" + 
+            "## EXIT Is Cube Held? : " + other.GetComponent<VRC_Pickup>().IsHeld
+            );
+    }
+}

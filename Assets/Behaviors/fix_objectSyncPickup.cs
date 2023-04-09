f55a32f148da2d02d7cb3479603bfcd83caa4b07
lr222gw <lowe.raivio@gmail.com>
2023-04-09 11:25:12 +0200

    CLN: Remove redundant function

--- Assets/Behaviors/fix_objectSyncPickup.cs
+++ Assets/Behaviors/fix_objectSyncPickup.cs
@@ -8,70 +8,16 @@ public class fix_objectSyncPickup : UdonSharpBehaviour
 {
     void Start()
     {
-        // justEntered         = false;
-        // wasInside           = false;
-        // justDropped         = false;
-        // justPicked          = false;
-        // changeOnDrop        = false;
     }
 
-
-    // // public bool justEntered;
-    // // public bool justDropped;
-    // // public bool justPicked;
-    // // public bool wasInside;
-
-
-    // public bool changeOnDrop;
-
-    // [UdonSynced]
-    // public bool isInside = false;
-
-    // public void removeCollider()
-    // {
-    //     if(gameObject.GetComponent<VRC_Pickup>().currentPlayer == null && isHeld)
-    //     {
-    //         Debug.Log("## VRC_Pickup.currentPlayer == null, But is it being held?");
-    //         SendCustomEventDelayedFrames(nameof(removeCollider),
-    //         2,
-    //         VRC.Udon.Common.Enums.EventTiming.LateUpdate);
-    //         return;
-    //     }
-
-    //     if( gameObject.GetComponent<VRC_Pickup>().currentPlayer != Networking.LocalPlayer &&
-    //         gameObject.GetComponent<VRC_Pickup>().currentPlayer != null)
-    //     {
-    //         Debug.Log("## removeCollider!");
-            
-    //         gameObject.GetComponent<Rigidbody>().detectCollisions = false;
-    //         // this.wasInside = true;
-    //     }
-    // }
     public void addCollider()
     {
-        // if(gameObject.GetComponent<VRC_Pickup>().currentPlayer != null) // && isHeld
-        // {
-        //     Debug.Log("## VRC_Pickup.currentPlayer == a player, But Shouldnt!");
-        //     SendCustomEventDelayedFrames(nameof(addCollider),
-        //     2,
-        //     VRC.Udon.Common.Enums.EventTiming.LateUpdate);
-        //     return;
-        // }
         
         if(gameObject.GetComponent<VRC_Pickup>().currentPlayer != Networking.LocalPlayer)
         {
             Debug.Log("## addCollider!");
             gameObject.GetComponent<Rigidbody>().detectCollisions = true;
-            // wasDropped = true;
         }
-        // if(Networking.IsOwner(Networking.LocalPlayer,gameObject))
-        // {
-        //     SendCustomNetworkEvent(
-        //         VRC.Udon.Common.Interfaces.NetworkEventTarget.All,
-        //         nameof(setWasDroppedTrue)
-        //     );
-        // }
-
     }
     public void setWasDroppedTrue()
     {
@@ -81,81 +27,6 @@ public class fix_objectSyncPickup : UdonSharpBehaviour
             nameof(addCollider)
         );
     }
-
-    // public void d_s_removeCollider()
-    // {
-        
-    //     SendCustomNetworkEvent(
-    //         VRC.Udon.Common.Interfaces.NetworkEventTarget.All,
-    //         nameof(s_removeCollider)
-    //     );   
-    // }
-    // public void s_removeCollider()
-    // {
-    //     Debug.Log("## s_Picked up");
-    //     // this.justPicked  = true;
-    //     // this.pickupReady = true;
-    //     isHeld = true;
-    //     removeCollider();
-    // }
-    // public void s_addCollider()
-    // {
-    //     Debug.Log("## s_Dropped ");
-    //     // this.justDropped = true; 
-    //     // this.dropReady   = true;
-    //     // isHeld = false;
-    //     addCollider();
-    // }
-
-    // public bool transitionIntoEnter = false;
-
-    
-
-    // public void s_tempActivateCollider()
-    // {
-    //     gameObject.GetComponent<Rigidbody>().detectCollisions = true;
-    //     transitionIntoEnter = true;
-    //     this.isInside = true; 
-
-        
-    //     // SendCustomEventDelayedFrames(nameof(addCollider),
-    //     //     2,
-    //     //     VRC.Udon.Common.Enums.EventTiming.LateUpdate
-    //     // );
-    // }
-
-    // public void ss_leavingTrigger()
-    // {
-    //     SendCustomNetworkEvent(
-    //         VRC.Udon.Common.Interfaces.NetworkEventTarget.All,
-    //         nameof(s_leavingTrigger)
-    //     );
-    // }
-
-    // public void s_leavingTrigger()
-    // {
-    //     gameObject.GetComponent<Rigidbody>().detectCollisions = true;
-    //     this.isInside = false; 
-        
-    //     // SendCustomEventDelayedFrames(nameof(addCollider),
-    //     //     2,
-    //     //     VRC.Udon.Common.Enums.EventTiming.LateUpdate
-    //     // );
-    // }
-
-    // public void ss_tempActivateCollider()
-    // {
-    //     SendCustomNetworkEvent(
-    //         VRC.Udon.Common.Interfaces.NetworkEventTarget.All,
-    //         nameof(s_tempActivateCollider)
-    //     );
-    // }
-
-    // // public bool pickupReady = false; 
-    // // public bool dropReady = false; 
-
-    // [UdonSynced]
-    // public bool isHeld = false; 
 
     public onTrigger_script myScript = null;
     
@@ -213,29 +84,15 @@ public class fix_objectSyncPickup : UdonSharpBehaviour
         myScript.doOnTriggerExit(gameObject.GetComponent<Collider>());
     }
 
-    // public void s_addCollider()
+    // public void setWasDroppedFalse_a()
     // {
-    //     addCollider();
+    //     this.wasDropped = false;
     // }
-
-
-    public void setWasDroppedFalse_a()
-    {
-        this.wasDropped = false;
-    }
 
     public void setWasDroppedFalse()
     {
-        setWasDroppedFalse_a();
-        // if(!gameObject.GetComponent<Collider>().attachedRigidbody.IsSleeping())
-        // {
-        //     SendCustomEventDelayedFrames(
-        //     nameof(SendCustomEventDelayedFrames),
-        //     1,
-        //     VRC.Udon.Common.Enums.EventTiming.LateUpdate);
-        // }else {
-        //     setWasDroppedFalse_a();
-        // }
+        // setWasDroppedFalse_a();
+        this.wasDropped = false;
     }
     public void s_setWasDroppedFalse()
     {
